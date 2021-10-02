55002ff675bfd08da7ae10a7418ca5d97217b2f1
Art Beckett <89126163+Bear1986@users.noreply.github.com>
2021-10-02 07:53:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+#while loops
+
+#while
+x = 0
+while x < 10:
+  print(x)
+  x += 1
+#will output 0 - 9
+
+print()
+
+#If
+y = 0
+if y < 10:
+  print(y)
+  y += 1
+#will out put 0 as its only exicuted once
+
+print()
+
+#For
+for k in range(10):
+  print(k)
+#it will like the while output 1-10
+
+print()
+
+j = 4
+while j > -4:
+   print(j)
+   j -= 1
+
+print()
